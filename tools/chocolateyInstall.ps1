4d3c17e212c29819163689751837194ae6a78459
Andy Helsby <21364480+Absoblogginlutely@users.noreply.github.com>
2018-10-18 18:56:15 -0400

    Installation script file

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'screenshotcaptor'
+$installerType = 'EXE'
+$url = 'http://www.donationcoder.com/Software/Mouser/screenshotcaptor/downloads/ScreenshotCaptorSetup.exe'
+$silentArgs = '/SILENT'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
