24eb6d7b75930ea0967c4e904d567d02eb85ce30
Andy Helsby <21364480+Absoblogginlutely@users.noreply.github.com>
2018-10-22 07:27:57 -0400

    Changed install command to install with no UI

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'screenshotcaptor'
 $installerType = 'EXE'
 $url = 'http://www.donationcoder.com/Software/Mouser/screenshotcaptor/downloads/ScreenshotCaptorSetup.exe'
-$silentArgs = '/SILENT'
+$silentArgs = '/verysilent /norestart'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
