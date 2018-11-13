6c87480338d64e22dab50b3bb92b47f9f2f977cf
Andy Helsby <21364480+Absoblogginlutely@users.noreply.github.com>
2018-11-13 08:36:19 -0500

    Updated to use splatting for parameters

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,13 @@
-﻿$packageName = 'screenshotcaptor'
-$installerType = 'EXE'
 $url = 'http://www.donationcoder.com/Software/Mouser/screenshotcaptor/downloads/ScreenshotCaptorSetup.exe'
-$silentArgs = '/verysilent /norestart'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+$packageName = 'screenshotcaptor'
+$checkSum = "E6DD2CC656E32F8CC77031C2D993F38C45C087CF56EB592484D9836C71DF8498"
+$packageArgs=@{
+	packageName = $packageName
+	installerType = 'EXE'
+	url=$url
+	silentArgs = '/verysilent /norestart'
+	validExitCodes = @(0)
+	checksumtype='sha256'
+	checksum=$checksum
+}
+Install-ChocolateyPackage @packageArgs
